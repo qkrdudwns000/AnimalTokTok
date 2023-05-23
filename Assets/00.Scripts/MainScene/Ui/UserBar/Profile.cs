0a0fc29e34f12d1b05621b9c99fa41b635f5f79b
ju-hyeon-kim <63406146+ju-hyeon-kim@users.noreply.github.com>
2023-05-23 21:48:23 +0900

    JH)0523_2
    ui작업

--- Assets/00.Scripts/MainScene/Ui/UserBar/Profile.cs
+++ Assets/00.Scripts/MainScene/Ui/UserBar/Profile.cs
@@ -6,5 +6,9 @@ public class Profile : MonoBehaviour
 {
     public GameObject ProfileChangeWindow;
 
-
+    public void OnClick()
+    {
+        //클릭하면 프로필변경 창이 뜨고 프로필을 변경할 수 있음
+        ProfileChangeWindow.SetActive(true);
+    }
 }
