1a71fd1550785f4e618e2f31ed6135ea958c61b0
ju-hyeon-kim <63406146+ju-hyeon-kim@users.noreply.github.com>
2023-05-28 22:47:57 +0900

    JH_0528
    랭킹UI 작업

--- Assets/00.Scripts/MainScene/Ui/ProfileChangeWindow.cs
+++ Assets/00.Scripts/MainScene/Ui/ProfileChangeWindow.cs
@@ -9,7 +9,8 @@ public class ProfileChangeWindow : MonoBehaviour
     public Transform Arrow;
     public Transform[] Images;
 
-    int ProfileImageNum = 0;
+    public Sprite SelectecdImage;
+    int NowProfileImageNum = 0;
 
     void OnEnable()
     {
@@ -17,12 +18,27 @@ public class ProfileChangeWindow : MonoBehaviour
         //ProfileImageNum = 0; // ProfileImageNum은 추후에 서버에 저장 필요
 
         // 현재 나의 프로필 이미지에 맞는 이미지에 화살표가 표시됨
-        MoveArrow(ProfileImageNum);
+        SelectImage(NowProfileImageNum);
     }
 
-    public void MoveArrow(int imagenum) // imagenum에 해당하는 이미지위에 화살표를 이동 시키는 함수 + 이미지들의 onclick
+    public void SelectImage(int imagenum) // imagenum에 해당하는 이미지위에 화살표를 이동 시키는 함수 + 이미지들의 onclick
     {
+        SelectecdImage = Images[imagenum].GetChild(0).GetComponent<Image>().sprite; // 스프라이트 저장
+        NowProfileImageNum = imagenum; // 이미지 번호 저장
+        SaveInServer();
+
         Arrow.SetParent(Images[imagenum]);
         Arrow.localPosition = new Vector3 (0, 100, 0);
+    }
+
+    void SaveInServer()
+    {
+        // 서버 연결 후 구현 필요
+    }
+
+    public void SaveProfile() // SaveButton Onclick
+    {
+        ProfileImage.sprite = SelectecdImage; // 프로필 이미지 변경
+        this.gameObject.SetActive(false); // 창 닫기
     }
 }
